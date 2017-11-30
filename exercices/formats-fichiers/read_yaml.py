07a88c15c040feda11f5be07ec5ba00ad3044e39
Renaud Dussurget <rdussurget@mercator-ocean.fr>
2017-11-30 11:01:28 +0100

    moved makina content from github (2)

--- exercices/formats-fichiers/read_yaml.py
+++
@@ -1,8 +0,0 @@
-import os
-import yaml
-
-filename = os.path.join(os.path.dirname(__file__), "example.yml")
-
-with open(filename) as fp:
-    data = yaml.load(fp)
-    print(data["technologies"][1])  # Django
--- exercices/stdlib/commandes.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-p1 = subprocess.Popen(["ps","aux"], stdout=subprocess.PIPE)
-p2 = subprocess.Popen(["grep", "java"], stdin=p1.stdout, stdout=subprocess.PIPE)
-p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-output, error = p2.communicate()
-print(output)
